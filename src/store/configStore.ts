0eb386bc61a194b15a3221b891a43200df6b4c81
Asva <ontrew@gmail.com>
2018-05-30 14:30:33 +0300

    Minor

--- src/store/configStore.ts
+++ src/store/configStore.ts
@@ -1,19 +1,19 @@
 import storage from './storage'
-import DemoPageConfig, {DemoPageMode} from '../components/DemoPage/DemoPageConfig'
+import DemoPageConfig, { DemoPageMode } from '../components/DemoPage/DemoPageConfig'
 
 const STORAGE_KEY = 'config'
 
 export default {
   _config: null,
-  
+
   get config (): DemoPageConfig {
-    if (! this._config) {
+    if (!this._config) {
       const data = storage.fetch(STORAGE_KEY) || new DemoPageConfig()
       this._config = new DemoPageConfig(
         data.mode,
         data.isFlat,
         data.searchText,
-        data.width
+        data.width,
       )
     }
     return this._config
