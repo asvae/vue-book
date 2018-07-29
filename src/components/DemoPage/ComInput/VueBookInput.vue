f9d796e083f397846d4ea67f46e22eb95baeb171
Asva <ontrew@gmail.com>
2018-07-29 22:18:49 +0300

    Remove commented out code

--- src/components/DemoPage/ComInput/VueBookInput.vue
+++ src/components/DemoPage/ComInput/VueBookInput.vue
@@ -66,60 +66,6 @@ export default {
     },
   },
 }
-//@Component
-//export default class VueBookInput extends Vue {
-//  temporaryValue: string = ''
-//
-//  @Prop({
-//    type: String,
-//    default: 'default',
-//  })
-//  type!: string
-//
-//  @Prop({
-//    type: String,
-//    default: 'default',
-//  })
-//  name!: string
-//
-//  @Prop({
-//    type: String,
-//    default: '',
-//  })
-//  value!: string
-//
-//  @Prop({
-//    type: String,
-//    default: '',
-//  })
-//  placeholder!: string
-//
-//  @Prop({
-//    type: String,
-//    default: '',
-//  })
-//  tooltip!: string
-//
-//  get valueProxy () {
-//    if (this.value !== this.temporaryValue) {
-//      this.temporaryValue = this.value
-//    }
-//
-//    return this.temporaryValue
-//  }
-//
-//  set valueProxy (value) {
-//    this.temporaryValue = value
-//
-//    this.$nextTick(() => {
-//      if (this.value !== this.temporaryValue) {
-//        this.temporaryValue = this.value
-//      }
-//    })
-//
-//    this.$emit('input', value)
-//  }
-//}
 </script>
 
 <style lang="scss">
