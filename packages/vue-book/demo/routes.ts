08454efa07732abd5dd4897a0ba7ac8f9602359a
igor.prokofiev <igorprodev1@gmail.com>
2020-10-22 16:21:19 +0300

    fix/demo major fixes

--- packages/vue-book/demo/routes.ts
+++ packages/vue-book/demo/routes.ts
@@ -1,52 +1,50 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 import { createComponent, createRoute, VueBookComponents } from '../src/app'
 import SeveralInstances from './SeveralInstances.vue'
 
-Vue.use(Router)
-Vue.use(VueBookComponents)
-
-const router = new Router({
-  routes: [
-    // An assortment of components.
-    createRoute({
+const routes = [
+  createRoute({
+    requireContext: require.context('./tree', true, /.vue$/),
+    path: '/demo',
+    hideFileExtensions: true,
+    // hideNavigation: true,TreeFileFactory
+  }),
+  // vue-book component mode testing
+  {
+    path: '/component-mode/:pathMatch(.*)*',
+    component: createComponent({
+      path: '/component-mode',
       requireContext: require.context('./tree', true, /.vue$/),
-      path: '/demo',
-      hideFileExtensions: true,
-      // hideNavigation: true,TreeFileFactory
-    }),
-    // vue-book component mode testing
-    {
-      path: '/component-mode/*',
-      component: createComponent({
-        path: '/component-mode',
-        requireContext: require.context('./tree', true, /.vue$/),
-        hideFileExtensions: true,
-      }),
-    },
-    // Test several vue-book instances in one page.
-    {
-      path: '/several-instances',
-      component: SeveralInstances,
-    },
-    // Internal components
-    createRoute({
-      requireContext: require.context('./../src', true, /.demo.vue$/),
-      path: '/src',
       hideFileExtensions: true,
     }),
-    // Test case where no demo files exist.
-    createRoute({
-      requireContext: require.context('./../src', true, /.test-empty.vue$/),
-      path: '/empty',
-      hideFileExtensions: true,
-    }),
-    {
-      path: '/*',
-      redirect: '/demo',
-    },
-  ],
+  },
+  // Test several vue-book instances in one page.
+  {
+    path: '/several-instances',
+    component: SeveralInstances,
+  },
+  // Internal components
+  createRoute({
+    requireContext: require.context('./../src', true, /.demo.vue$/),
+    path: '/src',
+    hideFileExtensions: true,
+  }),
+  // Test case where no demo files exist.
+  createRoute({
+    requireContext: require.context('./../src', true, /.test-empty.vue$/),
+    path: '/empty',
+    hideFileExtensions: true,
+  }),
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/demo',
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
 })
 
 export default router
